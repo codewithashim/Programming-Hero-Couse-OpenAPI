fad3325be32ea5877a045c9a76bbb97ca7f7047c
Ashim Rudra Paul <ashimrudrapaul23@gmail.com>
2022-09-16 16:39:44 +0600

    added all function

--- js/app.js
+++ js/app.js
@@ -7,19 +7,16 @@ const loadMilestone = async () => {
 
 const displayMilestone = async () => {
   const data = await loadMilestone()
-
   // ================== Display Milestone List =================
   const milstoneContainer = document.getElementById('milestoneContainer')
-
   data.forEach(milstoneData => {
     const div = document.createElement('div')
-    const milestonItem = milstoneData.modules
 
     div.innerHTML = `
         <div class="milestone border-b">
         <div class="flex">
-          <div class="checkbox"><input type="checkbox" /></div>
-          <div onclick="openMilstion(this)">
+          <div class="checkbox" onclick="markMilston(this, id)"><input type="checkbox" /></div>
+          <div onclick="openMilstion(this, ${milstoneData._id})">
             <p>
               ${milstoneData.name}
               <span><i class="fas fa-chevron-down"></i></span>
@@ -43,11 +40,9 @@ const displayMilestone = async () => {
 
 }
 
-const openMilstion = (milstonEliment) => {
+const openMilstion = (milstonEliment, id) => {
   const currentPanel = milstonEliment.parentNode.nextElementSibling;
   const shownPanel = document.querySelector('.show')
-
-
   const active = document.querySelector('.active')
 
   if (active && !milstonEliment.classList.contains('active')) {
@@ -58,8 +53,39 @@ const openMilstion = (milstonEliment) => {
   if (!currentPanel.classList.contains("show") && shownPanel) {
     shownPanel.classList.remove('show');
   }
-
   currentPanel.classList.toggle("show")
+
+  showMilestone(id)
 }
+
+const showMilestone = async (id) => {
+  const data = await loadMilestone()
+  console.log(data)
+
+  const milestoneImage = document.querySelector('.milestoneImage')
+  milestoneImage.style.opacity = "0";
+  milestoneImage.src = data[id].image
+
+  const milstoneTitle = document.querySelector('.title')
+  milstoneTitle.innerText = data[id].name
+
+  const milstoneDetails = document.querySelector('.details')
+  milstoneDetails.innerText = data[id].description
+}
+
+// listen for image load 
+const milestoneImage = document.querySelector('.milestoneImage')
+milestoneImage.onload = function () {
+  this.style.opacity = "1";
+}
+
+
+const markMilston = () => {
+  const doneList = document.querySelector('.doneList');
+  const milstonsList = document.querySelector('.milestones');
+
+
+}
+
 
 displayMilestone()
